a999d744416e71f6be1576b55594fc9a10daf12c
Pradyuman Sarswat <pradyumansarswat@gmail.com>
2024-08-13 16:32:11 +0530

    utils code api response apierrors

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);  
     } catch (error) {
-        console.log("MONGODB connection error", error)
+        console.log("MONGODB connection Failed", error)
         process.exit(1)
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv";
 import connectDB from './db/index.js'; 
+import { app } from "./app.js";
 
 
 
@@ -9,9 +10,15 @@ dotenv.config({
     path: './env'
 })
 
-connectDB();
-
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(` Server is running at port : ${process.env.PORT}`)
+    });
+})
+.catch((err)=>{
+    console.log("MONGO DB connection failed !!! ", err)
+})
 
 
 
